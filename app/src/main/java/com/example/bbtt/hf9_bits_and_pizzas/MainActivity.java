91fcb207d538dd689b3c895aa84e0147915b4198
codemass <worklette@yandex.ru>
2018-09-18 00:39:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bbtt/hf9_bits_and_pizzas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bbtt.hf9_bits_and_pizzas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
